d7cc2aa3e31e686bfcc72e5eed8d45c8dd540fb9
puri-ph4tt <puriphat001@gmail.com>
2024-01-25 09:42:45 +0700

    Refactor: order status

--- client/src/routes/status/+page.svelte
+++ client/src/routes/status/+page.svelte
@@ -5,26 +5,26 @@
 </script>
 
 <section>
-  <div class="mx-12 pt-10 p-4">
+  <div class="mx-12 pt-10 px-4">
     <span class="font-bold text-5 text-stone-800">รายการสั่งซื้อ</span>
   </div>
 
-  <div class="p-4 mx-12 pb-10">
-    <div class="relative overflow-x-auto shadow-md sm:rounded-md">
+  <div class="px-4 mx-12 mt-6 pb-10">
+    <div class="relative overflow-x-auto shadow-md rounded-md">
       <table class="w-full text-sm text-left rtl:text-right text-stone-500">
-        <thead class="text-xs text-white uppercase bg-orange-900">
+        <thead class="text-xs text-stone-900 uppercase bg-stone-300">
           <tr>
             <th scope="col" class="px-6 py-3"> ชื่อ </th>
             <th scope="col" class="px-6 py-3"> ที่อยู่ </th>
             <th scope="col" class="px-6 py-3"> สินค้า </th>
-            <th scope="col" class="px-6 py-3"> ขนาด(กิโลกรัม) </th>
+            <th scope="col" class="px-6 py-3"> ขนาด (กิโลกรัม) </th>
             <th scope="col" class="px-6 py-3"> สถานะ </th>
           </tr>
         </thead>
         <tbody>
           {#each data.order as order}
             <tr class="bg-white border-t border-stone-200 hover:bg-stone-50">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+              <th scope="row" class="px-6 py-4 font-medium text-stone-900 whitespace-nowrap">
                 {order.name}
               </th>
               <td class="px-6 py-4"> {order.address} </td>
